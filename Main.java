ca56254b5d96538da7ceb937d08d7998e3609676
samtell21 <samtell21@gmail.com>
2017-01-19 14:48:00 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-public class NewClass {
+public class Main {
     
     public static void main(String[] args)
     {
